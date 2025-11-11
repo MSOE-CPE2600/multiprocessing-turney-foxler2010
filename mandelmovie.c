041952a3e3aa358cae4d2c814189658d1f731269
foxler2010 <drew@drewssite.top>
2025-11-11 11:51:55 -0600

    start working on the actual movie program

---
+++ mandelmovie.c
@@ -0,0 +1,12 @@
+/*
+ * Filename: mandelmovie.c
+ * Description: Mandelbrot Set zoom animation generator
+ * Author: Drew Malone <malonea@msoe.edu>
+ * Created: 11/11/25
+ * Compile with: make
+ */
+
+int main(int argc, char *argv[])
+{
+    // TODO
+}
---
+++ mandelmovie.h
@@ -0,0 +1,18 @@
+/*
+ * Filename: mandelmovie.h
+ * Description: Header file for mandelmovie.c
+ * Author: Drew Malone <malonea@msoe.edu>
+ * Created: 11/11/25
+ * Compile with: make
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SCALE 0.0001
+#define X_COORD -1.249705
+#define Y_COORD 0.111053
+#define OUTFILE kool.jpg
+
+#define BIG_WIDTH 8000
+#define BIG_HEIGHT 8000
